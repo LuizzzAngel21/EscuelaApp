36300fdfe852a9b96923669bfd1a6a7f9f5e9331
LuizzzAngel21 <Luis_@LUIS_ANGEL>
2026-02-09 18:47:54 -0500

    Estudiante_Final_1

--- Escuela.Web/wwwroot/js/config.js
+++ Escuela.Web/wwwroot/js/config.js
@@ -1,3 +1,1 @@
-﻿// REEMPLAZA '5029' POR EL PUERTO QUE SÍ TE ABRIÓ SWAGGER
-// Y usa 127.0.0.1 en lugar de localhost
-const API_BASE_URL = "http://127.0.0.1:5200/api";
+﻿const API_BASE_URL = "http://127.0.0.1:5200/api";
