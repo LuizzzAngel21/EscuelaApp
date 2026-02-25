6d2a0f8a87f759c5e0c3ff11f9b64f7a6ca3a2e7
Kimmy2709 <kimberlysed07@gmail.com>
2026-02-25 18:42:06 -0500

    Dashboard de psicologo

---
+++ Escuela.Web/Controllers/PsicologoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Escuela.Web.Controllers
+{
+    public class PsicologoController : Controller
+    {
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+        
+    }
+}
